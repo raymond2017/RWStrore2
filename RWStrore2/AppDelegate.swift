1c66226fa88498af1e65735f686040b823143921
LuckyMonkey <raymond2017.cn@gmail.com>
2019-04-21 19:40:10 +0800

    Initial Commit

---
+++ RWStrore2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RWStrore2
+//
+//  Created by Raymond on 4/21/19.
+//  Copyright © 2019 Raymond. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RWStrore2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RWStrore2
+//
+//  Created by Raymond on 4/21/19.
+//  Copyright © 2019 Raymond. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
